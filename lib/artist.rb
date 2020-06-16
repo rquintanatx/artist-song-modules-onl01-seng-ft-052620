7c05677370fb4be0e09c0762008df81012d7931f
Robert Quintana <robertquintanatx@gmail.com>
2020-06-16 21:12:23 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,13 +19,11 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+# def self.reset_all
+#    self.all.clear
+#  end
 
-  def self.count
-    self.all.count
-  end
+
 
   def add_song(song)
     @songs << song
